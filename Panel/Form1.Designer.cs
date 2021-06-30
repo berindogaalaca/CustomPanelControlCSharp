93f688acafef058ed0fbb4e2576d886dc0c8111f
bdalaca <dogaalacaaa@gmail.com>
2021-07-01 00:47:26 +0300

    last version

---
+++ Panel/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myPanel1 = new Panel.MyPanel();
+            this.SuspendLayout();
+            // 
+            // myPanel1
+            // 
+            this.myPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.myPanel1.Location = new System.Drawing.Point(556, 187);
+            this.myPanel1.Name = "myPanel1";
+            this.myPanel1.Size = new System.Drawing.Size(201, 101);
+            this.myPanel1.TabIndex = 0;
+            this.myPanel1.Text = "myPanel1";
+            this.myPanel1.Click += new System.EventHandler(this.myPanel1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyPanel myPanel1;
+    }
+}
+
---
+++ Panel/MyPanel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Panel
+{
+    public partial class MyPanel : ContainerControl
+    {
+        private Rectangle Shape; 
+        private int x;
+        private int y;
+        /// <summary>
+        /// Componentin kurucu methodu background, size ve stilleri ayarlar
+        /// </summary>
+        public MyPanel()
+        {
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.UserPaint, true);
+
+            BackColor = Color.Transparent;
+            x = 200; y = 100;
+            Padding = new Padding(0,0, 0, 0);
+            this.Size = new Size(201, 101);
+            
+        }
+        /// <summary>
+        /// width, height değerlerine göre rectangle çizdirilir.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Shape = new Rectangle(0, 0, x, y);
+            Bitmap Bmp = new Bitmap(Width, Height);
+            Graphics Grp = Graphics.FromImage(Bmp);
+
+            Grp.SmoothingMode = SmoothingMode.HighQuality;
+
+            Grp.Clear(Color.Transparent);
+            Grp.DrawRectangle(new Pen(Color.FromArgb(180, 180, 180)), Shape);
+
+            Grp.Dispose();
+            e.Graphics.DrawImage((Image)Bmp.Clone(), 0, 0);
+            Bmp.Dispose();
+        }
+        /// <summary>
+        /// size değiştikçe width, height değerlerini değiştirerek componenti invalidate eder.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            x = this.Size.Width-1;
+            y = this.Size.Height-1;
+            this.Invalidate();
+        }
+    }
+}
